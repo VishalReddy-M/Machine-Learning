821809a369c94c6f6d6e494cf33ac85918d9d396
Vishal Reddy <vishalreddymadhini@gmail.com>
2026-01-25 17:51:21 +0530

    new changes

--- Random_forest.py
+++ Random_forest.py
@@ -26,4 +26,58 @@ correcting the mistake made by the previous model'''
     classification:gini,entropy
     regression:squared_error,poission
    7.random_state
-    default:none,limit:int>=40     '''
+    default:none,limit:int>=40'''
+
+#IMPLEMENTATION
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+from sklearn.tree import plot_tree
+df = pd.read_csv("Titanicdataset.csv")
+#print(df.head())
+#print(df.isna().sum())
+#print(df.info())
+df1=df[['Survived','Sex','Pclass','SibSp','Parch','Fare','Age']]
+df1["Age"] = df1["Age"].fillna(df1["Age"].mean())
+#print(df1.isna().sum())
+#preprocessing
+label = LabelEncoder()
+df1['Sex'] = label.fit_transform(df1['Sex'])
+print(df1.head())
+#feature engineering
+x=df1.drop("Survived",axis=1)
+y=df1["Survived"]
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=43)
+#model building
+model = RandomForestClassifier(
+    n_estimators=100,
+    max_depth=4,
+    criterion="gini",
+    min_samples_split=8,
+    min_samples_leaf=4,
+    random_state=42,
+    bootstrap=True
+)
+model.fit(x_train,y_train)
+y_pred = model.predict(x_test)
+print(confusion_matrix(y_test,y_pred))
+print(accuracy_score(y_test,y_pred))
+new_data = [[1,1,0,0,73,70]]
+prediction=model.predict(new_data)
+if prediction[0] == 1:
+    print("passenger is survived")
+else:
+    print("passenger is not survived")
+
+feature_names = x.columns.tolist()
+class_names = ['Not Survived', 'Survived']
+plt.figure(figsize=(20,10))
+plot_tree(model.estimators_[0], feature_names=feature_names, class_names=class_names, filled=True, rounded=True,fontsize=10)
+plt.show()
