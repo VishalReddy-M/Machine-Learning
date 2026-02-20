2ae5c4a56766f5bfbceca2e38aa69d57dd7b6a7f
Vishal Reddy <vishalreddymadhini@gmail.com>
2026-02-20 15:50:02 +0530

    new files

---
+++ Kmeansclustering.py
@@ -0,0 +1,5 @@
+#K = no of clusters
+# if K=2 then the data split into 2 groups
+# K is not default it has to be assigned before the implementation
+# cluster has own centroid
+# the data is divided into K clusters such that each data point belongs to the cluster with the nearest mean(centroid)
